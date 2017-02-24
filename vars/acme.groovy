6346fd2a5144096f69e608a5a31c9931ab4cbfcf
Ondrej <odolejsi@icloud.com>
2017-02-23 23:24:59 -0500

    Added var `acme` that has getName and setName and caution methods.

--- vars/acme.groovy
+++ vars/acme.groovy
@@ -7,6 +7,6 @@ class acme implements Serializable {
         name
     }
     def caution(message) {
-        echo "Hello, ${name}! CAUTION: ${message}"
+        println "Hello, ${name}! CAUTION: ${message}"
     }
 }
